fe0ce5b4bd0713a9b354dcd1e4f876f10bd7511d
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-03-11 12:06:16 -0300

    Adiciona geração aleatória e mensagem dinâmica no jogo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-alert("Boas vindas ao jogo do número secreto!")
+alert("Boas vindas ao jogo do número secreto");
+let numeroMaximo = 100;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+  chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+  }
+}
+
+let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+
+alert(
+  `Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativas}.`
+);
